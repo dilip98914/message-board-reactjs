ed107b6804813a0cbaa20da9ce1627c9dd570e6c
dilip gupta <dilip98914@gmail.com>
2018-12-27 21:34:27 +0530

    .

---
+++ build/precache-manifest.f46ab3bd47f30ae7d5dbc37fcdb0efef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b28dd69a9f920c83b1c",
+    "url": "/static/js/main.1b28dd69.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "1b28dd69a9f920c83b1c",
+    "url": "/static/css/main.93e193ba.chunk.css"
+  },
+  {
+    "revision": "464a7c970e1a9cfb0cb854bc01a3f8f0",
+    "url": "/index.html"
+  }
+];
